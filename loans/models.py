b114d648ea70d7df5f7961cfbf9b377286060a24
unChrkr <ubiratan.ckoster@hotmail.com>
2023-07-03 16:18:54 -0300

    feat: user and loan modal ready

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Loan(models.Model):
+    book = models.ForeignKey('books.Book', on_delete = models.PROTECT)
+    copy = models.ForeignKey('users.User', on_delete = models.PROTECT)
+    user = models.ForeignKey('copies.Copy', on_delete = models.PROTECT)
+    return_date = models.DateField()
+    loan_date = models.DateField()
+    expected_return = models.DateField()
+    delay = models.BooleanField()
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    books = models.ManyToManyField('Book', related_name = 'users')
+    username = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 127)
+    isAuth = models.BooleanField(default = True)
+    isAdmin = models.BooleanField(default = False)
+    book_history = models.ManyToManyField('Book', related_name= 'user')
