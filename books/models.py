4987ec9d0d73f6574b7a5d34dde0c3c54e1d9880
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-07-03 16:26:34 -0300

    fix: book models on right place

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.PROTECT, related_name="book_history")
+    copy_id = models.ForeignKey("copies.Copy", on_delete=models.CASCADE, related_name="book")
+    description = models.CharField(max_length=200)
+    is_available = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, unique=True)
