c4ac76c016f14886de49c26327debf8ed357e3a2
Rhyan Oliveira <rhyan1510@hotmail.com>
2023-07-04 14:25:47 -0300

    feat: relation copy/book

--- copies/models.py
+++ copies/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-class Copie(models.Model):
+class Copy(models.Model):
     # class Meta:
     #     ordering = ["id"]
-    quantity = models.IntegerField()
+    copyNumber = models.IntegerField()
+    book = models.ForeignKey("books.Book", on_delete=models.CASCADE, related_name="copy")
+    
--- copies/views.py
+++ copies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from copies.serializers import CopySerializer
+from .models import Copy
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+class CopyView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
