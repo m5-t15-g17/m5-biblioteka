7e4decc5576e6e537cb1e839b8df9622571cce62
unChrkr <ubiratan.ckoster@hotmail.com>
2023-07-04 16:06:35 -0300

    feat: create user ready

--- users/views.py
+++ users/views.py
@@ -2,13 +2,18 @@ from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
 from .permissions import IsAccountOwner
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.views import Request, status, Response
+ 
 
 
 
-class UserView(CreateAPIView):
+class UserView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def post(self, request: Request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
    
 
 class UserDetailView(RetrieveUpdateDestroyAPIView):
