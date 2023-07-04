b9a79f4299903ae9d827dda4887cf8f810f9deb5
Robson-Luna-BR <robson.luna@outlook.com>
2023-07-04 12:04:24 -0300

    feat: adiciona logica de criaçãod e datas

--- loans/models.py
+++ loans/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+import datetime
+from datetime import timedelta
 
 
 class Loan(models.Model):
-    book = models.ForeignKey('books.Book', on_delete = models.PROTECT)
-    copy = models.ForeignKey('users.User', on_delete = models.PROTECT)
-    user = models.ForeignKey('copies.Copy', on_delete = models.PROTECT)
-    return_date = models.DateField()
-    loan_date = models.DateField()
-    expected_return = models.DateField()
-    delay = models.BooleanField()
+    returnDate = datetime.datetime.now()
+    returnDate = returnDate + datetime.timedelta(7, 0)
+    # book = models.ForeignKey('books.Book', on_delete = models.PROTECT)
+    copy = models.ForeignKey("users.User", on_delete=models.PROTECT)
+    user = models.ForeignKey("copies.Copy", on_delete=models.PROTECT)
+    return_date = models.DateField(null=True)
+    loan_date = models.DateField(auto_now_add=True)
+    expected_return = models.DateField(default=returnDate)
+    delay = models.BooleanField(default=False)
