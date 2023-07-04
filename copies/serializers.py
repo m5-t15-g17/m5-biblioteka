12ce99391a3c3ef65c3cd04b0c241fafe28f36da
Rhyan Oliveira <rhyan1510@hotmail.com>
2023-07-04 18:31:31 -0300

    fix: copyNumber

--- copies/serializers.py
+++ copies/serializers.py
@@ -9,4 +9,4 @@ class CopySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Copy
-        fields = ["id", "quantity", "book_id"]
+        fields = ["id", "copyNumber", "book_id"]
