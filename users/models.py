c79763c0bb65f7fbb94fd0fcd009a8267a4aad54
unChrkr <ubiratan.ckoster@hotmail.com>
2023-07-04 14:30:34 -0300

    feat: fixed models user

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    books = models.ManyToManyField('Book', related_name = 'users')
+    books = models.ManyToManyField('books.Book', related_name = 'user')
     username = models.CharField(max_length = 100)
     email = models.CharField(max_length = 127)
     is_auth = models.BooleanField(default = True)
