cff5a31425b9d9c002abb9f4b4b0364d7f697e6c
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-07-04 16:08:05 -0300

    feat: url for books created

--- books/serializers.py
+++ books/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 
 from books.models import Book
+from users.serializers import UsersSerializer
 
 
 class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        book = Book
-        fields = ["id", "user", "description", "is_available", "name"]
+    user = UsersSerializer(read_only=True)
 
     def create(self, validated_data: dict) -> Book:
         return Book.objects.create(**validated_data)
+
+    class Meta:
+        model = Book
+        fields = ["id", "user", "description", "is_available", "name"]
 
     def update(self, validated_data: dict, instance: Book) -> Book:
         for key, value in validated_data.items():
