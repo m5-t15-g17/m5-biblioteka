41242ca06e2f51d44bcc9e5c369a159fe40a2f87
unChrkr <ubiratan.ckoster@hotmail.com>
2023-07-04 13:56:42 -0300

    feat: user model fixed

--- users/models.py
+++ users/models.py
@@ -7,4 +7,4 @@ class User(AbstractUser):
     email = models.CharField(max_length = 127)
     is_auth = models.BooleanField(default = True)
     is_admin = models.BooleanField(default = False)
-    book_history = models.ManyToManyField('Book', related_name = 'user')
+
