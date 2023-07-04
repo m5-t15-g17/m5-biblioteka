43f73bd309200742999248d3aebdccdf6e632c5a
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-07-04 14:20:50 -0300

    feat: views maybe done and models changes

--- books/views.py
+++ books/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+
+class BookView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # def perform_create(self, serializer):
+    #     return serializer.save(user_id=self.kwargs.)
