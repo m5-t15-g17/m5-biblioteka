498011b6876f3353a99da2e341f86d850df459d6
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-07-07 18:12:58 -0300

    feat: url for documentation update

--- biblioteKA/urls.py
+++ biblioteKA/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
@@ -25,11 +25,6 @@ urlpatterns = [
     path("api/", include("copies.urls")),
     path("api/", include("users.urls")),
     path("api/", include("books.urls")),
-
-    # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # path(
-    #     "api/docs/swagger/",
-    #     SpectacularSwaggerView.as_view(url_name="schema"),
-    #     name="swagger"),
-
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
 ]
