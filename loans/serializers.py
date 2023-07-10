6cbf5fce5b4b87504ee3e714c113061656a55f5e
Robson-Luna-BR <robson.luna@outlook.com>
2023-07-10 17:06:21 -0300

    fix: remove vários bugs da aplicação

--- loans/serializers.py
+++ loans/serializers.py
@@ -4,8 +4,8 @@ from .models import Loan
 class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
-        fields = ["id", "user_id", "copy_id", "return_date", "loan_date", "expected_return"]
-        read_only_fields = ["user_id", "copy_id", "return_date", "loan_date", "expected_return"]
+        fields = ["id", "user", "copy", "return_date", "loan_date", "expected_return"]
+        read_only_fields = ["user", "copy", "return_date", "loan_date", "expected_return"]
 
         def create(self, validated_data):
             return Loan.objects.create(**validated_data)
--- loans/views.py
+++ loans/views.py
@@ -12,77 +12,139 @@ from users.models import User
 from datetime import datetime
 from rest_framework.views import Request, Response, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+import pdb
+from datetime import timedelta
+from django.forms import model_to_dict
 
 
 class LoanView(generics.CreateAPIView):
     authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    lookup_url_kwarg = "pk"
 
+    print("1")
+    # pdb.set_trace()
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
+    print("2")
 
+    # pdb.set_trace()
     def get_queryset(self):
+        print("3")
+        # pdb.set_trace()
         queryset = super().get_queryset()
-        instance_user = get_object_or_404(User, pk=self.request.user.id)
+        instance_user = get_object_or_404(User, id=self.request.user.id)
+        print("4")
+        # pdb.set_trace()
 
         return queryset.filter(users=instance_user)
+    
+    # def post(self, request, *args, **kwargs):
+    #     print("estou no post")
+    #     user = get_object_or_404(User, id=self.request.user.id)
+    #     return super().post(request, *args, **kwargs)
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
+        print("estou no post")
         user = get_object_or_404(User, id=self.request.user.id)
 
-        if user.is_blocked:
-            return Response({"error": "User blocked"})
+        # if user.is_block:
+        #     print("estou no post2")
+        #     return Response({"error": "User blocked"})
+        # loans = user.loans.all()
 
-        loans = user.loan.all()
+        # for loan in loans:
+        #     print("estou no post3")
+        #     if loan.return_date < datetime.now():
+        #         serializer = UserSerializer(user, {"is_block": True}, partial=True)
+        #         print("estou no post4")
+        #         serializer.is_valid(raise_exception=True)
+        #         serializer.save()
+        #         print("estou no post5")
+        #         return Response({"error": "User blocked"})
+        # return super().post(request, *args, **kwargs)
+        return Response({"message":"Emprestimo feito com sucesso"})
+    def perform_create(self, serializer):
+        returnDate = datetime.now()
+        returnDate = returnDate + timedelta(7, 0)
+
+        user = get_object_or_404(User, id=self.request.user.id)
+        if user.is_block:
+            print("estou no post2")
+            return Response({"error": "User blocked"})
+        loans = user.loans.all()
 
         for loan in loans:
+            print("estou no post3")
             if loan.return_date < datetime.now():
-                serializer = UserSerializer(user, {"is_blocked": True}, partial=True)
+                serializer = UserSerializer(user, {"is_block": True}, partial=True)
+                print("estou no post4")
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
-
+                print("estou no post5")
                 return Response({"error": "User blocked"})
+        copy = get_object_or_404(Copy, id=self.kwargs.get("pk"))
+        copys = Loan.objects.filter(copy=copy)
+        print(user, "1", copy, "2", len(copys), "3", "////////////////////////")
+        # pdb.set_trace()
+        # for copy in copys:
+        #     print(copy,"*******************************************")
+        # pdb.set_trace()
+        # if copy.id == self.kwargs.get("pk"):
+        if copy.copyNumber == 0:
+            book = get_object_or_404(Book, id=copy.book)
+            serializer = BookSerializer(book, {"is_avaliable": False}, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response("no copies left", 400)
 
-    def perform_create(self, serializer):
-        returnDate = datetime.datetime.now()
-        returnDate = returnDate + datetime.timedelta(7, 0)
+        serializer = CopySerializer(data=copy, partial=True)
+        serializer.copyNumber = copy.copyNumber - 1
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        user = get_object_or_404(User, pk=self.request.user.id)
-        copy = get_object_or_404(Copy, pk=self.kwargs.get("pk"))
-        copys = copy.loan.all()
+        loanSerializer = LoanSerializer(copy=copy, expected_return=returnDate, user=user)
+        loanSerializer.is_valid(raise_exception=True)
 
-        for copy in copys:
-            if copy.id == self.kwargs.get("pk"):
-                if copy.copyNumber == 0:
-                    book = get_object_or_404(Book, pk=copy.book)
-                    serializer = BookSerializer(
-                        book, {"is_avaliable": False}, partial=True
-                    )
-                    serializer.is_valid(raise_exception=True)
-                    serializer.save()
-                    return Response("no copies left", 400)
+        # serializer.save(copy=copy, expected_return=returnDate, user=user)
+        # break
+        # copyDict = model_to_dict(serializer)
 
-                serializer = CopySerializer(
-                    copy, {"copyNumber": "copyNumber" - 1}, partial=True
-                )
-                serializer.is_valid(raise_exception=True)
-                serializer.save()
+        # return Response(copyDict)
+        return loanSerializer.save()
 
-                break
-
-        return serializer.save(copy=copy, expected_return=returnDate, user=user)
+    # def perform_create(self, serializer):
+    #     returnDate = datetime.datetime.now()
+    #     returnDate = returnDate + datetime.timedelta(days=7)
+    #     user = get_object_or_404(User, pk=self.request.user.id)
+    #     copy = get_object_or_404(Copy, pk=self.kwargs.get("pk"))
+    #     copys = copy.loan.all()
+    #     for copy in copys:
+    #         if copy.id == self.kwargs.get("pk"):
+    #             if copy.copyNumber == 0:
+    #                 book = get_object_or_404(Book, pk=copy.book.pk)
+    #                 book.is_available = False
+    #                 book.save()
+    #                 return Response("no copies left", status=400)
+    #             copy.copyNumber -= 1
+    #             copy.save()
+    #             break
+    #     serializer.save(copy=copy, expected_return=returnDate, user=user)
+    #     return Response(serializer.data, status=201)
 
     def patch(self, request: Request):
         user = get_object_or_404(User, id=self.request.user.id)
 
         self.check_object_permissions(request, user)
         loans = user.loan.all()
-        copy = get_object_or_404(Copy, pk=self.kwargs.get("pk"))
+        copy = get_object_or_404(Copy, id=self.kwargs.get("pk"))
         copys = copy.loan.all()
 
         for copy in copys:
             if copy.id == self.kwargs.get("pk"):
                 if copy.copyNumber == 0:
-                    book = get_object_or_404(Book, pk=copy.book)
+                    book = get_object_or_404(Book, id=copy.book)
                     serializer = BookSerializer(
                         book, {"is_avaliable": True}, partial=True
                     )
@@ -99,7 +161,7 @@ class LoanView(generics.CreateAPIView):
 
         for loan in loans:
             if loan.return_date < datetime.now():
-                serializer = UserSerializer(user, {"is_blocked": True}, partial=True)
+                serializer = UserSerializer(user, {"is_block": True}, partial=True)
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
 
