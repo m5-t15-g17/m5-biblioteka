36445102d238dad6a9b5ab8190b2c6e600b68389
Robson-Luna-BR <robson.luna@outlook.com>
2023-07-10 14:13:58 -0300

    feat: adiciona rota de loans a aplicação

--- biblioteKA/urls.py
+++ biblioteKA/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("api/", include("copies.urls")),
     path("api/", include("users.urls")),
     path("api/", include("books.urls")),
+    path("api/", include("loans.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
 ]
